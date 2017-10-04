4041ed443f3186f83024ede9a7877b6ec20ddcd8
Amalia Dunca <amaliadunca@Amalias-MacBook-Pro.local>
2017-10-04 17:40:47 +0200

    3 buttons need help! JS

--- script.js
+++ script.js
@@ -2,7 +2,26 @@ let char = document.querySelector("img.hero");
 let shakeButton = document.querySelector("#shake");
 shakeButton.addEventListener("click", makeShake);
 
-function makeShake(){
+function makeShake() {
     console.log("make shake");
     char.classList.toggle("shakeCSS");
 };
+
+/* jump character*/
+
+let jumpButton = document.querySelector("#jump");
+jumpButton.addEventListener("click", makeJump);
+
+function makeJump() {
+    console.log("make jump");
+    char.classList.toggle("jumpCSS");
+}
+/*walk*/
+
+let walkButton = document.querySelector("#walk");
+walkButton.addEventListener("click", makeWalk);
+
+function makeWalk() {
+    console.log("make walk");
+    char.classList.toggle("walkCSS");
+}
