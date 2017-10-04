7af2a4d92f564becb803cacbef5b08b5b85ef5fa
Amalia Dunca <amaliadunca@Amalias-MacBook-Pro.local>
2017-10-04 20:33:44 +0200

    animation project buttons

--- script.js
+++ script.js
@@ -43,10 +43,64 @@ fadeButton.addEventListener("click", makeFade);
 
 function makeFade() {
     console.log("make fade");
-    char.classList.toggle("fadeCSS")
+    char.classList.toggle("fadeCSS");
 
-};
+}
 
+/* fly*/
+
+let flyButton = document.querySelector("#fly");
+flyButton.addEventListener("click", flyHigh);
+
+function flyHigh () {
+    console.log("fly high/low");
+    char.classList.toggle("flyCSS");
+}
+ /* move30*/
+
+let move30Button = document.querySelector("#move30");
+move30Button.addEventListener("click", move);
+
+function move () {
+    console.log("move30");
+    char.classList.toggle("move30CSS");
+}
+
+/* move from30*/
+
+let movechar = document.querySelector("#moveOut");
+
+movechar.addEventListener("click", moveFrom30);
+
+function moveFrom30() {
+    console.log("moveFrom30");
+    char.classList.toggle("moveFrom");
+
+}
+
+/*play jumping*/
+
+let jumping = document.querySelector("#play");
+
+jumping.addEventListener("click", jumpBackFourth);
+
+function jumpBackFourth() {
+    console.log("jumpBackFourth");
+    char.classList.toggle("playCSS");
+
+}
+
+/*glow*/
+
+let glowing = document.querySelector("#glow");
+
+glowing.addEventListener("click", glowIt);
+
+function glowIt() {
+    console.log("glow it baby");
+    char.classList.toggle("glowCSS");
+
+}
 
 
 
